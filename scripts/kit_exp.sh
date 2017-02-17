8865e8ebac6055c3998580114f9c4ce9d8c41137
kitstar <kitseraph@outlook.com>
2017-02-17 21:06:18 +0800

    nip

--- scripts/kit_exp.sh
+++ scripts/kit_exp.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-### constant parameter
-work_path="~/run/dnn"
+### Constantis
+username="kit"
+password="19870817"
+
+host_ip="192.168.1.120"
+machine_list=machine_list.txt
+script_path="~/run/dnn"
+python_path="~/anaconda2"
 
 
 
+### UDFs
+install_python()
+{
+    echo "TODO"
+}
+
+
+
+### Functions
 setup () 
 {
-    echo "Call Setup"
+    install_python
+}
+
+
+copy_script()
+{
+    while IFS= read -r line; do
+        if [ "${line}" != "${host_ip}" ]
+        then
+            echo "copying scripts to ${line}"
+            ssh ${username}@{line} "mkdir ${script_path}"
+            scp -r -C "${script_path}/*" "${username}@${line}:${script_path}"
+        fi
+    done < ${machine_list}
 }
 
 
@@ -22,8 +50,12 @@ usage ()
 # main ()                  #
 
 case "${1}" in
-"setup") setup
+"setup" | "st") setup
 ;;
+
+"copyscript" | "cs") copy_script
+;;
+
 
 "") usage
 ;;
