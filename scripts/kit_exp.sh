91cbd856b845dd7ff5b6eb9c35c636e16a76b676
kitstar <kitseraph@outlook.com>
2017-02-18 21:17:33 +0800

    ni

--- scripts/kit_exp.sh
+++ scripts/kit_exp.sh
@@ -6,7 +6,8 @@ password="19870817"
 
 host_ip="192.168.1.120"
 machine_list=machine_list.txt
-script_path="~/run/dnn"
+script_path="~/exp/dnn"
+run_path="~/run/dnn"
 python_path="~/anaconda2"
 
 
@@ -25,15 +26,30 @@ setup ()
     install_python
 }
 
+auth()
+{
+    if [ ! -f "/.ssh/id_rsa.pub" ]; then
+        ssh-keygen -t rsa
+    fi
+
+    while IFS= read -r line; do
+        if [ "${line}" != "${host_ip}" ] && [ "${line:0:1}" != "#" ]
+        then
+            echo "copying authority to ${line}"
+            ssh-copy-id -i "~/.ssh/id_rsa.pub" "${line}"
+        fi
+    done < ${machine_list}
+}
+
 
 copy_script()
 {
     while IFS= read -r line; do
-        if [ "${line}" != "${host_ip}" ]
+        if [ "${line}" != "${host_ip}" ] && [ "${line:0:1}" != "#" ]
         then
             echo "copying scripts to ${line}"
-            ssh ${username}@{line} "mkdir ${script_path}"
-            scp -r -C "${script_path}/*" "${username}@${line}:${script_path}"
+            ssh ${username}@${line} "mkdir -p ${script_path}"
+            scp -r -C "${script_path}/*" "${username}@${line}:${run_path}/"
         fi
     done < ${machine_list}
 }
@@ -50,6 +66,9 @@ usage ()
 # main ()                  #
 
 case "${1}" in
+"auth") auth
+;;
+
 "setup" | "st") setup
 ;;
 
