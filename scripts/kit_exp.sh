7dc7b890b406a05f184aab017306586d46ba4685
kitstar <kitseraph@outlook.com>
2017-02-19 15:54:34 +0800

    fix ssh usage in while loop.

--- scripts/kit_exp.sh
+++ scripts/kit_exp.sh
@@ -6,8 +6,8 @@ password="19870817"
 
 host_ip="192.168.1.120"
 machine_list=machine_list.txt
-script_path="~/exp/dnn"
-run_path="~/run/dnn"
+script_path="/home/kit/exp/dnn"
+run_path="/home/kit/run/dnn"
 python_path="~/anaconda2"
 
 
@@ -28,28 +28,36 @@ setup ()
 
 auth()
 {
-    if [ ! -f "/.ssh/id_rsa.pub" ]; then
+    if [ ! -f "~/.ssh/id_rsa.pub" ]; then
         ssh-keygen -t rsa
     fi
 
     while IFS= read -r line; do
-        if [ "${line}" != "${host_ip}" ] && [ "${line:0:1}" != "#" ]
+#        if [ "${line}" != "${host_ip}" ] && [ "${line:0:1}" != "#" ]
+        if [ "${line:0:1}" != "#" ]
         then
             echo "copying authority to ${line}"
-            ssh-copy-id -i "~/.ssh/id_rsa.pub" "${line}"
+            ssh-copy-id -i ~/.ssh/id_rsa.pub "${line}"
         fi
     done < ${machine_list}
+}
+
+
+mkdirs()
+{
+    ssh -n ${1}@${2} "mkdir -p ${3}"
 }
 
 
 copy_script()
 {
     while IFS= read -r line; do
-        if [ "${line}" != "${host_ip}" ] && [ "${line:0:1}" != "#" ]
+        if [ "${line:0:1}" != "#" ]
         then
             echo "copying scripts to ${line}"
-            ssh ${username}@${line} "mkdir -p ${script_path}"
-            scp -r -C "${script_path}/*" "${username}@${line}:${run_path}/"
+            #ssh ${username}@${line} "mkdir -p ${run_path}"
+            mkdirs $username $line $run_path
+            scp -r -C ${script_path}/* ${username}@${line}:${run_path}
         fi
     done < ${machine_list}
 }
